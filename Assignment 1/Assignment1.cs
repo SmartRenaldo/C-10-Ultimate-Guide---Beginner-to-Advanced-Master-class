16421f5ee07a1b052018085e7f734d4320cda2c1
SmartRenaldo <correctgavin@gmail.com>
2022-11-14 22:14:02 +1030

    add assignment 1

---
+++ Assignment 1/Assignment1.cs
@@ -0,0 +1,14 @@
+public class Assignment1
+{
+    static void Main()
+    {
+        double pi = 3.14159;
+        int radius = 5;
+
+        double area = pi * radius * radius;
+
+        //output
+        System.Console.WriteLine(area);
+        System.Console.ReadKey();
+    }
+}
