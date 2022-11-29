66d8ceb9450d9b09303da07782e78a0ae20e1fa0
RENALDO LUMIENT <renaldo@lumient.com.au>
2022-11-29 21:42:52 +1030

    create and raise event

---
+++ Delegates & Events/01_WhatIsEvent/ClassLibrary/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    //delegate type
+    public delegate void MyDelegateType(int num1, int num2);
+
+    //publisher
+    public class Publisher
+    {
+        private MyDelegateType myDelegateType;
+
+        //step 1: create event
+        public event MyDelegateType myEvent
+        {
+            add
+            {
+                myDelegateType += value;
+            }
+            remove
+            { 
+                myDelegateType -= value;
+            }
+        }
+
+        //step 2: raise event
+        public void RaiseEvent(int a, int b)
+        {
+            this.myDelegateType(a, b);
+        }
+    }
+}
