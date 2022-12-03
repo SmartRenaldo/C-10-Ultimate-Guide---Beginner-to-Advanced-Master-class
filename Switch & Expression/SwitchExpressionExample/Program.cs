443fdf8327afa983f664c556790ab4f9033d0d52
SmartRenaldo <correctgavin@gmail.com>
2022-12-04 09:26:31 +1030

    switch & expression

---
+++ Switch & Expression/SwitchExpressionExample/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SwitchExpressionExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 0 - 3
+            int animal = 10;
+            string result;
+
+            //switch expression
+            result = animal switch
+            {
+                0 => "Cat",
+                1 => "Dog",
+                2 => "Mouse",
+                3 => "Pig",
+                _ => "No case available",
+            };
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
