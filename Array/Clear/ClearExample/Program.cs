221eff0f36a7fba7aad3664d3c80bfff1b44bd8d
RENALDO LUMIENT <renaldo@lumient.com.au>
2022-12-04 22:53:21 +1030

    Clear

---
+++ Array/Clear/ClearExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClearExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            //create array
+            int[] a = new int[] { 10, 20, 30, 40, 50, 60, 70 };
+
+            //clear elements of array
+            Array.Clear(a, 3, 3);
+
+            //display array
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+
+            //create array
+            int[] b = new int[] { 10, 20, 30, 40, 50, 60, 70 };
+
+            //clear elements of array
+            Array.Clear(b, 0, b.Length);
+
+            //display array
+            foreach (var item in b)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
