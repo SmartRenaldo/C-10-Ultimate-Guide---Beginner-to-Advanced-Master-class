c700f61d5e90a68a6e1e77ed2fb89e8360d8c2ec
SmartRenaldo <correctgavin@gmail.com>
2022-12-08 23:19:39 +1030

    array of objects

---
+++ Array/Array of Objects/ArrayOfObjectsExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace ArrayOfObjectsExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            //create array of objects
+            Employee[ ] employees = new Employee[ ]
+            {
+                new Employee() { EmpID = 1, EmpName = "Tom" },
+                new Employee() { EmpID = 2, EmpName = "Tim" },
+                new Employee() { EmpID = 3, EmpName = "Tony" }
+            };
+
+            //foreach loop for array of objects
+            foreach(Employee item in employees)
+            {
+                if (item.EmpID >= 102)
+                {
+                    Console.WriteLine(item.EmpID + ", " + item.EmpName);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
