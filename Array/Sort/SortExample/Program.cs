ea3a385d6c761d5e75cb330c97e1d9080e5cdd0c
SmartRenaldo <correctgavin@gmail.com>
2022-12-08 22:42:23 +1030

    sort

---
+++ Array/Sort/SortExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SortExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            //create array
+            int[] a = new int[] { 46, 94, 35, 24, 6, 69 };
+
+            //sort ascending order
+            Array.Sort(a);
+
+            //display array after sort
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
