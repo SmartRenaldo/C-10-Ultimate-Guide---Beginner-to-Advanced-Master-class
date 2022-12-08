29e93ede5ffeaeafbb9983b85dc72965ebb448f8
SmartRenaldo <correctgavin@gmail.com>
2022-12-08 22:44:36 +1030

    Reverse

---
+++ Array/Reverse/ReverseExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ReverseExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            //create array
+            int[] a = new int[] { 46, 94, 35, 24, 6, 69 };
+
+            //sort ascending order
+            Array.Sort(a);
+
+            //display array after sort
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+
+            //reverse the array (it becomes descending order)
+            Array.Reverse(a);
+
+            //display array after reverse
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
