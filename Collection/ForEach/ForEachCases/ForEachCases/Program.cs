8e436d2471dccd2107629bff107f799f45a55f09
RENALDO LUMIENT <renaldo@lumient.com.au>
2023-01-06 22:51:15 +1030

    add ForEach practice for List

---
+++ Collection/ForEach/ForEachCases/ForEachCases/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ToArrayForEachExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            //create list
+            List<string> myFriends = new List<string>() { "Scott", "Allen", "James", "Jones" };
+
+            //ForEach method
+            Console.WriteLine("\nForEach method:");
+            myFriends.ForEach(f => { Console.WriteLine(f); });
+            Console.ReadKey();
+        }
+    }
+}
