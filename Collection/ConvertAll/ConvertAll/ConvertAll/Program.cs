0629dc76b1d47aa3dae795cb43630d615ddec559
RENALDO LUMIENT <renaldo@lumient.com.au>
2023-01-07 22:54:24 +1030

    add test cases for List.ConvertAll method

---
+++ Collection/ConvertAll/ConvertAll/ConvertAll/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConvertAllExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            //create source collection
+            List<int> intCollection = new List<int>() { 1, 8, 2, 7 };
+
+            //read each value into lambda expression; convert the same value into char
+            List<char> charCollection = intCollection.ConvertAll(n => Convert.ToChar(n + 65));
+
+            //print the result collection
+            foreach (char item in charCollection)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
