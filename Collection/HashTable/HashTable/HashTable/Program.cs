d784be0652ab766943627535be6023b5851e69b7
RENALDO LUMIENT <renaldo@lumient.com.au>
2023-01-08 13:36:08 +1030

    add test cases for Hashtable

---
+++ Collection/HashTable/HashTable/HashTable/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+
+namespace HashtableExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            //create a hashtable
+            Hashtable employees = new Hashtable()
+            {
+                { 102, "Smith" },
+                { 105, "James" },
+                { 103, "Allen" },
+                { 101, "Scott" },
+                { 104, "Jones" },
+                { "hello", 10.934 }
+            };
+
+            //Add element
+            employees.Add(106, "Anna");
+
+            //Remove element
+            employees.Remove(103);
+
+            //foreach
+            foreach (DictionaryEntry item in employees)
+            {
+                Console.WriteLine(item.Key + ", " + item.Value);
+            }
+
+            //search for specific key
+            bool flag = employees.ContainsKey(105);
+            Console.WriteLine("\n105 exists: " + flag);
+
+            if (flag)
+            {
+                Console.WriteLine(employees[105]);
+            }
+
+            //search for specific value
+            bool v = employees.ContainsValue("Scott");
+            Console.WriteLine("\nScott exists: " + v);
+
+            //Keys
+            Console.WriteLine("\nKeys:");
+            foreach (var item in employees.Keys)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Values
+            Console.WriteLine("\nValues:");
+            foreach (var item in employees.Values)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
