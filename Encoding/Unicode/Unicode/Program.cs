29b9e643844da0e02721d9a12dec9d2761c06bb7
RENALDO LUMIENT <renaldo@lumient.com.au>
2023-01-28 16:34:49 +1030

    add example of Unicode encoding

---
+++ Encoding/Unicode/Unicode/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace UnicodeExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            string mytext = "ﺱ";
+            Console.WriteLine(mytext);
+            string mytext2 = "\u0543";
+            Console.WriteLine(mytext2);
+
+            string sentence = "Harry Potter is a series of seven fantasy novels written by British author J. K. Rowling.";
+            byte[] bytes = System.Text.Encoding.Unicode.GetBytes(sentence); //string to byte[]
+            string sentence2 = System.Text.Encoding.Unicode.GetString(bytes); //byte[] to string
+            Console.WriteLine(sentence2);
+
+            Console.ReadKey();
+        }
+    }
+}
