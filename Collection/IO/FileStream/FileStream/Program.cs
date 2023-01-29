dcb51cc3f627ad3152e06825bb8997d263d70c1d
RENALDO LUMIENT <renaldo@lumient.com.au>
2023-01-29 23:01:20 +1030

    add practice for FileStream

---
+++ Collection/IO/FileStream/FileStream/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace FileStreamExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            string filePath = @"c:\practice\dog.txt";
+            FileInfo fileInfo = new FileInfo(filePath);
+
+            FileStream fileStream1 = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+            FileStream fileStream2 = File.Create(filePath);
+            FileStream fileStream3 = File.Open(filePath, FileMode.Create, FileAccess.Write);
+            FileStream fileStream4 = File.OpenWrite(filePath);
+            FileStream fileStream5 = fileInfo.Create();
+            FileStream fileStream6 = fileInfo.Open(FileMode.Create, FileAccess.Write);
+            FileStream fileStream7 = fileInfo.OpenWrite();
+
+        }
+    }
+}
