2cf1f4b48ff9b31f7b15078a7cf59e2baa149286
RENALDO LUMIENT <renaldo@lumient.com.au>
2023-01-30 23:33:48 +1030

    add practice for StreamWriter

---
+++ Collection/IO/StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace StreamWriterReaderExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            string filePath = @"c:\practice\europe.txt";
+            FileInfo fileInfo = new FileInfo(filePath);
+            //FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+
+            //4 ways to create new object of StreamWriter
+            //StreamWriter streamWriter = new StreamWriter(filePath);
+            //StreamWriter streamWriter = new StreamWriter(fileStream);
+            //StreamWriter streamWriter = fileInfo.AppendText();
+            using (StreamWriter streamWriter = fileInfo.CreateText())
+            {
+                streamWriter.WriteLine("The pig (Sus domesticus), often called swine, hog, or domestic pig ");
+                //some code here
+                streamWriter.WriteLine("Cattle (Bos taurus) are large, domesticated, cloven-hooved, herbivores");
+                //some code here
+                streamWriter.WriteLine("The horse (Equus ferus caballus) is a domesticated, one-toed, hoofed mammal");
+                //streamWriter.Close(); //optional
+            }
+
+            Console.WriteLine("animal.txt created");
+            Console.ReadKey();
+        }
+    }
+}
