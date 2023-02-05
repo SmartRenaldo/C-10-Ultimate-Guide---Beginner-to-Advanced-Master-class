625fc9a1c78051b87f40ee3e6dd104159e7844df
RENALDO LUMIENT <renaldo@lumient.com.au>
2023-02-05 12:07:01 +1030

    add practice for FileScopedNamespace

---
+++ C# 9 & 10 New Feature/FileScopedNamespace/FileScopedNamespace/Dean.cs
@@ -0,0 +1,5 @@
+﻿namespace College;
+
+internal class Dean
+{
+}
---
+++ C# 9 & 10 New Feature/FileScopedNamespace/FileScopedNamespace/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+namespace College;
+using System.Linq;
+
+class Student
+{
+}
+
+class Teacher
+{
+}
