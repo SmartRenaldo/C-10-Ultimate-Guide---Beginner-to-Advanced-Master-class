7b4ead49b4d26801e441f73772424cccbbd8ba59
RENALDO LUMIENT <renaldo@lumient.com.au>
2023-02-06 20:28:04 +1030

    practice record

---
+++ C# 9 & 10 New Feature/Record/Record/Program.cs
@@ -0,0 +1,13 @@
+﻿public record Person(int Id, string Name, int Age);
+
+class Program
+{
+    static void Main()
+    {
+        Person person1 = new Person(1, "John", 20);
+        Person person2 = new(2, "Scott", 22);
+        Console.WriteLine($"{person1.Name}, {person1.Age}");
+        Console.WriteLine($"{person2.Name}, {person2.Age}");
+        Console.ReadKey();
+    }
+}
